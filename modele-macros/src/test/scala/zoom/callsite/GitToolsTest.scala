4d5ebb952a4f706e558ba839aaf158170e7491b8
François Sarradin <francois@univalence.io>
2018-07-23 09:47:44 +0200

    add some tests on get git

--- modele-macros/src/test/scala/zoom/callsite/GitToolsTest.scala
+++ modele-macros/src/test/scala/zoom/callsite/GitToolsTest.scala
@@ -2,19 +2,34 @@ package zoom.callsite
 
 import java.io.File
 import java.nio.charset.StandardCharsets
-import java.nio.file.{ Files, Path }
+import java.nio.file.{Files, Path}
 
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.revwalk.RevCommit
-import org.scalatest.{ FunSuiteLike, Matchers }
+import org.scalatest.{FunSuiteLike, Matchers}
 
 class GitToolsTest extends FunSuiteLike with Matchers {
 
   import GitTools._
 
+  val root: Path = Files.createTempDirectory("testGit")
+  val git: Git = Git.init().setDirectory(root.toFile).call()
+
+  test("should not get git for unmanaged directory") {
+    val unmanagedRoot: Path = Files.createTempDirectory("testGit")
+
+    val maybeGit = getGit(unmanagedRoot.toFile)
+
+    maybeGit should not be defined
+  }
+
+  test("should get git for managed directory") {
+    val maybeGit = getGit(root.toFile)
+
+    maybeGit should be(defined)
+  }
+
   test("a new file should not be clean") {
-    val root: Path = Files.createTempDirectory("testGit")
-    val git: Git = Git.init().setDirectory(root.toFile).call()
     val file: File = root.resolve("newFile").toFile
 
     file.createNewFile()
@@ -23,8 +38,6 @@ class GitToolsTest extends FunSuiteLike with Matchers {
   }
 
   test("a committed new file should be clean") {
-    val root: Path = Files.createTempDirectory("testGit")
-    val git: Git = Git.init().setDirectory(root.toFile).call()
     val file: File = root.resolve("newFile").toFile
 
     file.createNewFile()
@@ -35,8 +48,6 @@ class GitToolsTest extends FunSuiteLike with Matchers {
   }
 
   test("should get last commit id from a new committed file") {
-    val root: Path = Files.createTempDirectory("testGit")
-    val git: Git = Git.init().setDirectory(root.toFile).call()
     val file: File = root.resolve("newFile").toFile
     file.createNewFile()
     git.add().addFilepattern("newFile")
@@ -49,22 +60,18 @@ class GitToolsTest extends FunSuiteLike with Matchers {
   test(
     "should get last commit id from a committed file even after some modifications on this file"
   ) {
-      val root: Path = Files.createTempDirectory("testGit")
-      val git: Git = Git.init().setDirectory(root.toFile).call()
-      val file: File = root.resolve("newFile").toFile
-      file.createNewFile()
-      git.add().addFilepattern("newFile")
-      val initialCommit: RevCommit =
-        git.commit().setMessage("commit message yolo 1").call()
+    val file: File = root.resolve("newFile").toFile
+    file.createNewFile()
+    git.add().addFilepattern("newFile")
+    val initialCommit: RevCommit =
+      git.commit().setMessage("commit message yolo 1").call()
 
-      Files.write(file.toPath, "file contents".getBytes(StandardCharsets.UTF_8))
+    Files.write(file.toPath, "file contents".getBytes(StandardCharsets.UTF_8))
 
-      lastCommitIdOf(file) should be(initialCommit.name())
-    }
+    lastCommitIdOf(file) should be(initialCommit.name())
+  }
 
   test("should get last commit id after committed a file twice") {
-    val root: Path = Files.createTempDirectory("testGit")
-    val git: Git = Git.init().setDirectory(root.toFile).call()
     val file: File = root.resolve("newFile").toFile
     file.createNewFile()
     git.add().addFilepattern("newFile")
@@ -80,8 +87,6 @@ class GitToolsTest extends FunSuiteLike with Matchers {
   }
 
   test("should get a path to root of the repo from a file") {
-    val root: Path = Files.createTempDirectory("testGit")
-    val git = Git.init().setDirectory(root.toFile).call()
     val dir = root.resolve("a/b/c/d").toFile
     dir.mkdirs()
 
