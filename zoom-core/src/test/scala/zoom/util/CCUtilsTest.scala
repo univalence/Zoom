39c453e7e2b93a97d7040eb24fd2481c60f04722
François Sarradin <francois@univalence.io>
2018-08-17 09:22:40 +0200

    add more tests in CCUtilsTest

--- zoom-core/src/test/scala/zoom/util/CCUtilsTest.scala
+++ zoom-core/src/test/scala/zoom/util/CCUtilsTest.scala
@@ -4,20 +4,39 @@ import org.scalatest.{FunSuiteLike, Matchers}
 
 class CCUtilsTest extends FunSuiteLike with Matchers {
 
+  val toMap: AnyRef ⇒ Map[String, String] = CCUtils.getCCParams2
+
   test("should get map of simple entity") {
     val entity = CaseClassSimple("Hello")
 
-    val result = CCUtils.getCCParams2(entity)
+    val result = toMap(entity)
 
     result should have size 1
     result("field") should be("Hello")
+  }
+
+  test("should get map of entity with defined option") {
+    val entity = CaseClassWithOption(Some("Hello"))
+
+    val result = toMap(entity)
+
+    result should have size 1
+    result("field") should be("Hello")
+  }
+
+  test("should get map of entity with empty option") {
+    val entity = CaseClassWithOption(None)
+
+    val result = toMap(entity)
+
+    result shouldBe empty
   }
 
   test("should get map of entity with present subentity") {
     val sub_entity = CaseClassSimple("Hello")
     val entity     = CaseClassWithEmbed(Some(sub_entity))
 
-    val result = CCUtils.getCCParams2(entity)
+    val result = toMap(entity)
 
     result should have size 1
     result("sub_entity.field") should be("Hello")
@@ -26,7 +45,7 @@ class CCUtilsTest extends FunSuiteLike with Matchers {
   test("should get map of entity with absent subentity") {
     val entity = CaseClassWithEmbed(None)
 
-    val result = CCUtils.getCCParams2(entity)
+    val result = toMap(entity)
 
     result shouldBe empty
   }
@@ -34,5 +53,5 @@ class CCUtilsTest extends FunSuiteLike with Matchers {
 }
 
 case class CaseClassSimple(field: String)
-
+case class CaseClassWithOption(field: Option[String])
 case class CaseClassWithEmbed(sub_entity: Option[CaseClassSimple])
